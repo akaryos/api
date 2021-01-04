f5714081a0ba9ce0facb36b3f9fae7c3ad9c9bf1
Samuel Durante <hyossha@gmail.com>
2021-01-04 16:54:25 -0300

    Enabling cors

--- core/server.ts
+++ core/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import 'express-async-errors'
 import express from 'express'
+import cors from 'cors'
 
 import './database'
 import routes from './routes'
@@ -8,6 +9,7 @@ import errorHandler from './middlewares/errorHandler'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler)
