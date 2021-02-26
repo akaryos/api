d0843cf749111021eb82d3ad59f21985e0939c46
Samuel Durante <hyossha@gmail.com>
2021-02-26 19:43:50 -0400

    Storing database the use storage of user

--- core/services/UploadFileService.ts
+++ core/services/UploadFileService.ts
@@ -1,8 +1,10 @@
 import { injectable, inject } from 'tsyringe'
 import { getRepository } from 'typeorm'
 
-import File from '../models/File'
 import IStorageProvider from '../providers/StorageProvider/models/IStorageProvider'
+import User from '../models/User'
+import File from '../models/File'
+import AppError from '../errors/AppError'
 
 interface Request {
   user_id: string
@@ -21,11 +23,18 @@ class UploadFileService {
   ) {}
 
   public async execute ({ user_id, name, original, type, size, url }: Request): Promise<File> {
+    const userRepository = getRepository(User)
     const filesRepository = getRepository(File)
 
+    const user = await userRepository.findOne({ where: { id: user_id } })
     const file = filesRepository.create({ user_id, name, original, type, size, url })
 
+    if (user.storage >= 1E8) throw new AppError('You not have space for store this')
+
     await this.storageProvider.saveFile(name)
+    await userRepository.update(user_id, {
+      storage: file.size + user.storage
+    })
     await filesRepository.save(file)
 
     return file
